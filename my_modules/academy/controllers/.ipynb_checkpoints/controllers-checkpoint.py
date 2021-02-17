d020044d36d360944db27e6fa5173bca2b728da7
Dario19999 <dario_gow3@hotmail.com>
2021-02-17 02:06:42 +0000

    bad fix fix (no warnings hopefully)

--- my_modules/academy/controllers/.ipynb_checkpoints/controllers-checkpoint.py
+++ my_modules/academy/controllers/.ipynb_checkpoints/controllers-checkpoint.py
@@ -5,20 +5,20 @@ from odoo import http
 class Academy(http.Controller):
     @http.route('/academy/academy/', auth='public', website=True)
     def index(self, **kw):
-        Teachers = http.request.env['academy.teachers']
+        Teachers = http.request.env['academy.teachers'].search([])
         return http.request.render('academy.index', {
-            'teachers': Teachers.search([])
+            'teachers':Teachers
         })
 
-    @http.route('/academy/academy/objects/', auth='public')
-    def list(self, **kw):
-        return http.request.render('academy.listing', {
-            'root': '/academy/academy',
-            'objects': http.request.env['academy.academy'].search([]),
-        })
+#     @http.route('/academy/academy/objects/', auth='public')
+#     def list(self, **kw):
+#         return http.request.render('academy.listing', {
+#             'root': '/academy/academy',
+#             'objects': http.request.env['academy.academy'].search([]),
+#         })
 
-    @http.route('/academy/academy/objects/<model("academy.academy"):obj>/', auth='public')
-    def object(self, obj, **kw):
-        return http.request.render('academy.object', {
-            'object': obj
-        })
+#     @http.route('/academy/academy/objects/<model("academy.academy"):obj>/', auth='public')
+#     def object(self, obj, **kw):
+#         return http.request.render('academy.object', {
+#             'object': obj
+#         })
